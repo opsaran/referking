62f7e592d5e37b1cfb9952bca544ee6b70ed405f
OP Saran <84232627+opsaran@users.noreply.github.com>
2025-04-04 12:10:27 +0530

    Update shopify.js

--- web/shopify.js
+++ web/shopify.js
@@ -1,18 +1,16 @@
 import '@shopify/shopify-api/adapters/node';
-import pkg from '@shopify/shopify-app-express';
-const { ShopifyExpress } = pkg;
-
+import shopifyApp from '@shopify/shopify-app-express';
 import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const shopify = ShopifyExpress({
+export const shopify = shopifyApp({
   api: shopifyApi({
     apiKey: process.env.SHOPIFY_API_KEY,
     apiSecretKey: process.env.SHOPIFY_API_SECRET,
     scopes: ['read_customers', 'read_orders', 'write_discounts'],
     hostName: process.env.HOST.replace(/^https?:\/\//, ''),
-    apiVersion: '2025-04', // use latest API version
+    apiVersion: '2025-04',
     isEmbeddedApp: true,
   }),
   auth: {
