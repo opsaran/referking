1c3ce90aed431603804594e66960db95986e0ca6
OP Saran <84232627+opsaran@users.noreply.github.com>
2025-04-04 12:18:04 +0530

    Delete web directory

--- web/graphqlUtils.js
+++
@@ -1,6 +0,0 @@
-import { shopifyApi } from "@shopify/shopify-api";
-
-export async function runGraphQLQuery(session, query, variables = {}) {
-  const client = new shopifyApi.clients.Graphql({ session });
-  return await client.query({ data: { query, variables } });
-}
