8070d5ba60b7d89fb8d7c590a58cf338d82b688d
OP Saran <84232627+opsaran@users.noreply.github.com>
2025-04-04 11:41:14 +0530

    Delete web directory

--- web/utils/linkGenerator.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export function generateReferralCode() {
-  return crypto.randomBytes(4).toString('hex');
-}
-
-export function createReferralLink(shopDomain, code) {
-  return `https://${shopDomain}/?ref=${code}`;
-}
